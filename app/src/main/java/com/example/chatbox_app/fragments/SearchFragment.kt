8b1e22dcc8fc1fa343f6626911c42f4f002f9ebe
Mypc <sarbani2101@gmail.com>
2025-02-07 00:20:23 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/chatbox_app/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/chatbox_app/fragments/SearchFragment.kt
@@ -24,6 +24,7 @@ class SearchFragment : Fragment() {
     private val searchResults = mutableListOf<ChatItem>()
     private val currentUserId = FirebaseAuth.getInstance().currentUser?.uid
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
