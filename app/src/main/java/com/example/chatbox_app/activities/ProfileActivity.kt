cf082e76f123e3cc5f9f033816f7bf00c6cc9d9f
Mypc <sarbani2101@gmail.com>
2025-02-10 16:11:24 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/chatbox_app/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/chatbox_app/activities/ProfileActivity.kt
@@ -17,6 +17,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import java.io.ByteArrayOutputStream
 
+@Suppress("DEPRECATION")
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
@@ -114,6 +115,7 @@ class ProfileActivity : AppCompatActivity() {
         startActivityForResult(intent, PICK_IMAGE_REQUEST)
     }
 
+    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)}\n      with the appropriate {@link ActivityResultContract} and handling the result in the\n      {@link ActivityResultCallback#onActivityResult(Object) callback}.")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
@@ -166,7 +168,7 @@ class ProfileActivity : AppCompatActivity() {
                             locationText.text = user.address.ifEmpty { "N/A" }
 
                             // Load profile image from Base64
-                            if (!user.profileImage.isNullOrEmpty()) {
+                            if (user.profileImage.isNotEmpty()) {
                                 try {
                                     val decodedBytes = Base64.decode(user.profileImage, Base64.DEFAULT)
                                     val bitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
